2fff22e0a375805d126459db7ed4ce9339fdb6ec
mostafa <mb0411717@gmail.com>
2024-04-12 19:52:32 +0200

    first upload

---
+++ age.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int age;
+  
+  cin >> age;
+
+  int age_in_days = age * 365;
+  int age_in_hours = age_in_days * 60;
+
+  cout << " Age in days is: " << age_in_days << " day\n";
+  cout << " Age in hours is: " << age_in_hours << " hours\n";
+
+  return 0;
+}
